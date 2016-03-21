a0b66e2c39c8bdab7f636ef8f37db50665aedc79
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-21 18:07:03 -0400

    formatting for new house, added new char add functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,7 +8,17 @@ class CharactersController < ApplicationController
   end
 
   def new
-
+    @character = Character.new
+    @houses = House.all
   end
 
+  def create
+    @character = Character.create!(character_params)
+    redirect_to @character
+  end
+
+  private
+  def character_params
+    require(:character).permit(:name, :gender, :photo_url, :primary_weapon, :quote, :status, :house_id)
+  end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -16,6 +16,16 @@ class HousesController < ApplicationController
     redirect_to @house
   end
 
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to @house
+  end
+
   private
   def house_params
     params.require(:house).permit(:name, :region, :seat, :seat_url, :sigil_url, :motto, :great_house, :allegiance)
