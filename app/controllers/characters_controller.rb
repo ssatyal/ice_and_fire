e40f5bd9b8b5bc12b065a596de723fa665681e68
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-21 13:25:50 -0400

    added new house view details

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,4 +6,9 @@ class CharactersController < ApplicationController
   def show
     @character = Character.find(params[:id])
   end
+
+  def new
+
+  end
+
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,4 +6,18 @@ class HousesController < ApplicationController
   def show
     @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = @house.create!(house_params)
+    redirect_to @house
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :region, :seat, :seat_url, :sigil_url, :motto, :great_house, :allegiance)
+  end
 end
